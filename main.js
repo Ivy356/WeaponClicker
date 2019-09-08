9e93560f5aac5c46bf359f30d86449f8b74c66fa
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-09-08 13:40:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,132 @@
+function openTab(evt, pageName){
+	var i, tabcontent, tablinks
+	tabcontent = document.getElementsByClassName("tabcontent")
+	for(i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
+	tablinks = document.getElementsByClassName("tablinks");
+	for(i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	document.getElementById(pageName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+/*
+----Beginign of MainPage section of the javascript----
+*/
+//Initialize all global variables that are relevent to the game
+var totalBPS = 0;
+var bullets = 0;
+var bulletstext = document.getElementById("bullets");
+var clickStr = 1;
+var clickCost = 100;
+var gunPrice = 10;
+var gunTotal = 0;
+var gunBPS = 1;
+var powergunPrice = 100;
+var powergunTotal = 0;
+var powergunBPS = 5;
+var riflePrice = 1000;
+var rifleTotal = 0;
+var rifleBPS = 25;
+var gunUpgrade1 = false
+	
+function addBullets() { //function for clicking
+	bullets = bullets + clickStr;
+	//console.log(getCookie("bullets"))l;
+	document.getElementById("bullets").innerHTML = bullets.toFixed(1) + ' Bullets ';
+}
+
+function buyDavid() { //function for buying more davids
+	if(points >= davidPrice) {
+		points = points - davidPrice;
+		davidTotal = davidTotal + 1;
+		davidPrice = Math.ceil(10 * 1.15**davidTotal);
+		document.getElementById("david").innerHTML = 'Buy a David for ' + davidPrice + ' Lines of code';
+		document.getElementById("davidAmmount").innerHTML = 'you have ' + davidTotal + ' Davids';
+		document.getElementById("davidProduce").innerHTML = 'Writing ' + (davidLPS * davidTotal).toFixed(1) + ' lines of code per second';
+	}
+}
+
+function buyIzzy() { //Function for buying more izzys
+	if(points >= izzyPrice) {
+		points = points - izzyPrice;
+		izzyTotal = izzyTotal + 1;
+		izzyPrice = Math.ceil(100 * 1.15**izzyTotal);
+		document.getElementById("izzy").innerHTML = 'Buy an izzy for ' + izzyPrice + ' Lines of code';
+		document.getElementById("izzyAmmount").innerHTML = 'you have ' + izzyTotal + ' Izzys';
+		document.getElementById("izzyProduce").innerHTML = 'Writing ' + (izzyLPS * izzyTotal).toFixed(1) + ' lines of code per second';
+	}
+}
+
+function buyNicole() {
+	if(points >=nicolePrice) {
+		points = points - nicolePrice;
+		nicoleTotal = nicoleTotal + 1;
+		nicolePrice = Math.ceil(1100 * 1.15**nicoleTotal);
+		document.getElementById("nicole").innerHTML = 'Buy a Nicole for ' + nicolePrice + ' Lines of code';
+		document.getElementById("nicoleAmmount").innerHTML = 'you have ' + nicoleTotal + ' Nicoles';
+		document.getElementById("nicoleProduce").innerHTML = 'Writing ' + (nicoleLPS * nicoleTotal).toFixed(1) + ' Lines of code per second';
+	}
+}
+
+window.setInterval(function() { //Adds together all the Lines of Code and then updates the elements in the HTML
+	  points = (points + (davidTotal * davidLPS) + (izzyTotal * izzyLPS) + (nicoleTotal * nicoleLPS));
+		totalLPS = ((davidTotal * davidLPS) + (izzyTotal * izzyLPS) + (nicoleLPS * nicoleTotal));
+		document.getElementById("codepersec").innerHTML = totalLPS.toFixed(1) + ' Lines per second'
+		document.getElementById("points").innerHTML = points.toFixed(1) + ' Lines of code';
+		document.cookie = "points=" + points.toFixed(1);
+}, 1000); // dont change this to anything other than 1000 lol
+
+window.setInterval(function() {
+	document.getElementById("points").innerHTML = points.toFixed(1) + ' Lines of code';
+}, refreshRateVar);
+
+window.setInterval(function() { //Saves game data every 15 seconds
+	localStorage.setItem("linesofcode", points);
+	localStorage.setItem("davids", davidTotal);
+	localStorage.setItem("izzys", izzyTotal);
+	localStorage.setItem("nicoles", nicoleTotal);
+	console.log("Game Saved");
+}, 15000);
+
+
+/*
+----This section represents the Upgrades page of the javascript----
+*/
+function checkUpgrades() {
+	console.log(davidredbullUpgrade);
+	if(davidTotal >= 1 && davidredbullUpgrade != true) {
+		var davidredbullElements = document.getElementsByClassName("davidRedbull");
+		for (i = 0; i < davidredbullElements.length; i++){
+			davidredbullElements[i].style.display = "inline";
+		}
+	}
+}
+
+function davidRedBull() {
+	var davidredbullCost = 100
+	if(points >= davidredbullCost) {
+		points = points - davidredbullCost;
+		davidredbull = true;
+		localStorage.setItem("davidredbull", true);
+		davidLPS = davidLPS * 2;
+		var davidredbullElements = document.getElementsByClassName("davidRedbull");
+		for (i = 0; i < davidredbullElements.length; i++){
+			davidredbullElements[i].style.display = "none";
+		}
+	}
+}
+
+function clickHarder() { // Upgrade click ability
+
+	if (points >= clickCost) {
+		points = points - clickCost;
+		clickStr = clickStr + 1;
+		clickCost = clickCost*2.75;
+		localStorage.setItem("clickStr", clickStr); localStorage.setItem("clickCost", clickCost);
+		document.getElementById("clickCostDisplay").innerHTML = "Current level: " + clickStr.toFixed(1) + " Cost for next level: " + clickCost.toFixed(1);
+	}
+}
