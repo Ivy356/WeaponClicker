7a2765547d4c61f82489123c9134000f335751fd
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-09-10 13:06:54 -0600

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,22 +1,22 @@
 function numbers (labelValue) {
 
     // Nine Zeroes for Billions
-    return Math.abs(Number(labelValue)) >= 1.0e+9
+    return Math.abs(Number(labelValue)) >= 1e9
 
-    ? Math.abs(Number(labelValue)) / 1.0e+9 + "billion"
+    ? Math.abs(Number(labelValue)) / 1e9 + "billion"
     // Six Zeroes for Millions 
-    : Math.abs(Number(labelValue)) >= 1.0e+12
+    : Math.abs(Number(labelValue)) >= 1e12
 
-    ? Math.abs(Number(labelValue)) / 1.0e+12 + "trillion"
+    ? Math.abs(Number(labelValue)) / 1e12 + "trillion"
     // Three Zeroes for Thousands
-    : Math.abs(Number(labelValue)) >= 1.0e+15
+    : Math.abs(Number(labelValue)) >= 1e15
 
-    ? Math.abs(Number(labelValue)) / 1.0e+15 + "quadrillion"
+    ? Math.abs(Number(labelValue)) / 1e15 + "quadrillion"
       
     // Three Zeroes for Thousands
-    : Math.abs(Number(labelValue)) >= 1.0e+18
+    : Math.abs(Number(labelValue)) >= 1e18
 
-    ? Math.abs(Number(labelValue)) / 1.0e+18 + "quintillion"
+    ? Math.abs(Number(labelValue)) / 1e18 + "quintillion"
 
     : Math.abs(Number(labelValue));
 
