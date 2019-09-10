769a83e6d03db4bba09d675c0541eb8e296c6246
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-09-10 13:15:37 -0600

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,4 +1,4 @@
-$scope.MoneyFormat = function (labelValue) 
+$scope.BulletFormat = function (labelValue) 
                     {
                           // 18 Zeroes for Quintillions
                           return Math.abs(Number(labelValue)) >= 1.0e+18
@@ -21,8 +21,8 @@ $scope.MoneyFormat = function (labelValue)
                                : Math.abs(Number(labelValue));
                    }
 
-  $scope.rep.won = $scope.MoneyFormat($scope.rep.won);
-            $scope.outlook.rem = $scope.MoneyFormat($scope.outlook.rem);
-            $scope.rep.expectedAmount = $scope.MoneyFormat($scope.rep.expectedAmount);
-            $scope.rep.potential = $scope.MoneyFormat($scope.rep.potential);
-            $scope.rep.quota = $scope.MoneyFormat($scope.rep.quota);
+  $scope.rep.won = $scope.BulletFormat($scope.rep.won);
+            $scope.outlook.rem = $scope.BulletFormat($scope.outlook.rem);
+            $scope.rep.expectedAmount = $scope.BulletFormat($scope.rep.expectedAmount);
+            $scope.rep.potential = $scope.BulletFormat($scope.rep.potential);
+            $scope.rep.quota = $scope.BulletFormat($scope.rep.quota);
