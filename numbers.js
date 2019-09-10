f61c1439872fd987740deab1e6d070e4f0d564d2
Ivy356 <54084140+Ivy356@users.noreply.github.com>
2019-09-10 13:14:31 -0600

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,23 +1,28 @@
-function numbers (labelValue) {
+$scope.MoneyFormat = function (labelValue) 
+                    {
+                          // 18 Zeroes for Quintillions
+                          return Math.abs(Number(labelValue)) >= 1.0e+18
 
-    // Nine Zeroes for Billions
-    return Math.abs(Number(labelValue)) >= 1e9
+                               ? Math.abs(Number(labelValue)) / 1.0e+18 + "quintillion"
+    
+                          // 15 Zeroes for Quadrillions
+                          return Math.abs(Number(labelValue)) >= 1.0e+15
 
-    ? Math.abs(Number(labelValue)) / 1e9 + "billion"
-    // Six Zeroes for Millions 
-    : Math.abs(Number(labelValue)) >= 1e12
+                               ? Math.abs(Number(labelValue)) / 1.0e+15 + "quadrillion"
+                               // 12 Zeroes for Trillions
+                               : Math.abs(Number(labelValue)) >= 1.0e+12
 
-    ? Math.abs(Number(labelValue)) / 1e12 + "trillion"
-    // Three Zeroes for Thousands
-    : Math.abs(Number(labelValue)) >= 1e15
+                               ? Math.abs(Number(labelValue)) / 1.0e+12 + "trillion"
+                               // 9 Zeroes for Billions
+                               : Math.abs(Number(labelValue)) >= 1.0e+9
 
-    ? Math.abs(Number(labelValue)) / 1e15 + "quadrillion"
-      
-    // Three Zeroes for Thousands
-    : Math.abs(Number(labelValue)) >= 1e18
+                               ? Math.abs(Number(labelValue)) / 1.0e+9 + "billion"
 
-    ? Math.abs(Number(labelValue)) / 1e18 + "quintillion"
+                               : Math.abs(Number(labelValue));
+                   }
 
-    : Math.abs(Number(labelValue));
-
-}
+  $scope.rep.won = $scope.MoneyFormat($scope.rep.won);
+            $scope.outlook.rem = $scope.MoneyFormat($scope.outlook.rem);
+            $scope.rep.expectedAmount = $scope.MoneyFormat($scope.rep.expectedAmount);
+            $scope.rep.potential = $scope.MoneyFormat($scope.rep.potential);
+            $scope.rep.quota = $scope.MoneyFormat($scope.rep.quota);
