5c3dc8acf121171f18ea2b5e31029bf1c024d308
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-10 19:38:31 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,8 +1,32 @@
 $(function() {
 
-
 var numbers = [];
 
 $.getJSON('numbers.json', function(data) {
        $.each(data.numbers, function(i, f) {
+              "",
+              " thousand",
+              " million",
+              " billion",
+              " trillion",
+              " quadrillion",
+              " quintillion",
+              " sextillion",
+              " septillion",
+              " octillion",
+              " nonillion",
+              " decillion",
+              " undecillion",
+              " duodecillion",
+              " tredecillion",
+              " quattuordecillion",
+              " quinquadecillion",
+              " sedecillion",
+              " septendecillion",
+              " octodecillion",
+              " novendecillion",
+              " vigintillion"
+          }
+      }
+  }
     
